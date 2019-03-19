598299b19693497cd64c585aa3387fddaf5b2ecb
David Stout <dws24520@yahoo.com>
2019-03-19 00:08:23 -0400

    Added a editor script for creating the game board. Probably won't need the materials but for now it helps visualize different spaces

---
+++ Assets/Scripts/Editor/GameBoardEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace LaserBattle
+{
+    [CustomEditor(typeof(GameBoard))]
+    public class GameBoardEditor : Editor
+    {
+        GameBoard board;
+
+        private void OnEnable()
+        {
+            board = target as GameBoard;
+        }
+
+                int materialIndex = 0;
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if(GUILayout.Button("Create Board"))
+            {
+                for(int i = 0; i < board.boardSize.x; i++)
+                {
+                    for (int j = 0; j < board.boardSize.y; j++)
+                    {
+                        Vector3 v = new Vector3(i * 1 + board.spacer * i, 0.0f, j * 1 + board.spacer * j);
+                        GameObject go = Instantiate(board.gameSpace, v, Quaternion.identity, board.transform);
+                        go.GetComponent<Renderer>().material = board.colors[materialIndex];
+                        AdjustMaterialIndex();
+                    }
+                    AdjustMaterialIndex();
+                }
+                
+            }
+        }
+
+        private void AdjustMaterialIndex()
+        {
+            materialIndex++;
+            if (materialIndex > 1)
+                materialIndex = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LaserBattle
+{
+    public class GameBoard : MonoBehaviour
+    {
+        public float spacer;
+        public GameObject gameSpace;
+        public Vector2 boardSize;
+        public Material[] colors;
+    }
+}
