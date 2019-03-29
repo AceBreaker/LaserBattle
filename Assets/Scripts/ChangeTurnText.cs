b03ab376816988c9c62dc43717a5efd75aee6115
David Stout <dws24520@yahoo.com>
2019-03-28 22:43:36 -0400

    Added feedback for whose turn it is

---
+++ Assets/Scripts/ChangeTurnText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LaserBattle
+{
+    public class ChangeTurnText : MonoBehaviour
+    {
+        UnityEngine.UI.Text turnText;
+
+        public void ChangeText()
+        {
+            if (turnText.text == "Blue Turn")
+                turnText.text = "Red Turn";
+            else if (turnText.text == "Red Turn")
+                turnText.text = "Blue Turn";
+        }
+    }
+}
