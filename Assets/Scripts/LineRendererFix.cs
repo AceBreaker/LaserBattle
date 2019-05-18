ad723166fa6bad1ecd4b8f7e45eeb2455bb13ca7
David Stout <dws24520@yahoo.com>
2019-05-18 19:05:56 -0400

    Updated the laser to have a texture, and fixed a weird bug where it was bouncing oddly off some of the mirrors.

---
+++ Assets/Scripts/LineRendererFix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererFix : MonoBehaviour {
+
+    public float lineWidth;
+
+	// Use this for initialization
+	void Start () {
+		LineRenderer rend = GetComponent<LineRenderer>();
+        rend.widthMultiplier = lineWidth;
+	}
+}
