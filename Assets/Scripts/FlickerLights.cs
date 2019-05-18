e2bcad63bb985db9fe43cac258dbeca03b7abdfa
David Stout <dws24520@yahoo.com>
2019-05-18 17:51:01 -0400

    Added particles to the cores and updated their material

---
+++ Assets/Scripts/FlickerLights.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLights : MonoBehaviour {
+
+    public Light light;
+
+    public float minFlicker;
+    public float maxFlicker;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float flickerAmount1 = Random.Range(minFlicker, maxFlicker);
+        float flickerAmount2 = Random.Range(minFlicker, maxFlicker);
+        light.intensity = flickerAmount1 > flickerAmount2 ? flickerAmount1 : flickerAmount2;
+	}
+}
---
+++ Assets/Scripts/WaveMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour {
+
+    public Vector3 movementAxis;
+    public float speed;
+    public float magnitude;
+    public float frequency;
+
+    Vector3 startPosition;
+
+    public bool waveOn = true;
+
+    float time;
+    float startTime;
+    private void Start()
+    {
+        startTime = Time.time;
+        startPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //transform.position += movementAxis * Mathf.Sin((time +=Time.deltaTime*speed)) * magnitude;
+        time += Time.time - startTime;
+        transform.localPosition = startPosition + (movementAxis * speed * magnitude * Mathf.Sin(Time.time * frequency));
+        //transform.localPosition = startPosition + (movementAxis * speed * magnitude * Mathf.Sin(frequency * time));
+    }
+
+    public void ToggleWave()
+    {
+        ToggleWave(!waveOn);
+    }
+
+    public void ToggleWave(bool on)
+    {
+        waveOn = on;
+    }
+}
